6382cef4e6fb72bbf62898507a490c569ffd17d9
Naeem A <merzanaeem007@gmail.com>
2023-05-20 23:11:42 +0200

    type

--- naeemmrz.py
+++ naeemmrz.py
@@ -58,7 +58,7 @@ with col2:
     st.title(NAME)
     st.write(DESCRIPTION)
     st.download_button(
-        label=" 📄 Download Resume",
+        label=" 📄 Download CV",
         data=PDFbyte,
         file_name=resume_file.name,
         mime="application/octet-stream",
